5766c00321d5c7ab7d8c99d36c52c138d07ada16
Thijs Schreijer <thijs@thijsschreijer.nl>
2017-09-15 14:58:42 +0200

    work done

--- lib/resty/healthcheck.lua
+++ lib/resty/healthcheck.lua
@@ -15,16 +15,28 @@ local sleep = ngx.sleep
 local tinsert = table.insert
 local tremove = table.remove
 local utils = require("resty.healthcheck.utils")
+local resty_lock = require ("resty.lock")
 
 -- constants
-local LOG_PREFIX  = "[healthcheck] "
-
+local LOG_PREFIX = "[healthcheck] "
+local SHM_PREFIX = "lua-resty-healthcheck:"
 
 -- defaults
 local DEFAULT_TIMEOUT = 2
 local DEFAULT_INTERVAL = 1
 local DEFAULT_WAIT_MAX = 0.5
 local DEFAULT_WAIT_INTERVAL = 0.010
+
+local EVENTS = setmetatable({
+  --"add",        -- no 'add' because adding will result in `(un)healthy` events
+  "remove",
+  "healthy",
+  "unhealthy",
+},{
+  __index = function(self, key)
+    error(("'%s' is not a valid event name"):format(tostring(key)))
+  end
+})
 
 
 
@@ -74,7 +86,8 @@ Timer management:
   - fully internal
   - 2 timers, one for healthy, one for unhealthy
   - using lock mechanism from upstream-healthcheck library to ensure independence
-  - GC-able timers, or manual timer-stopping
+  - GC-able timers, or manual timer-stopping ==> to be tested! overall GC,
+    because the event library may also hold on to the healthchecker objects
 
 
 SHM storage
@@ -84,6 +97,20 @@ SHM storage
  - for now serialize as json in shm
 
 --]]
+
+
+
+-- Non-performing serialization for now...
+-- serialize a table to a string
+local function serialize(t)
+  return cjson.encode(t)
+end
+
+-- deserialize a string to a table
+local function deserialize(s)
+  return cjson.decode(s)
+end
+
 
 
 local checker = {}
@@ -98,20 +125,158 @@ local checker = {}
 -- @param (optional) healthy a boolean value indicating the initial state
 -- @return true on success, nil+err on failure
 function checker:add_target(ip, port, healthy)
+  ip   = tostring(assert(ip, "no ip address provided"))
+  port = tostring(assert(ip, "no port number provided"))
+  healthy = not not healthy   -- force to boolean
+
+  local lock, err, ok, err2, target_list
   
-  -- TODO: implement
+  lock, err = resty_lock.new(self.shm, {
+                exptime = 10,  -- timeout after which lock is released anyway
+                timeout = 5,   -- max wait time to acquire lock
+              })
+  if not lock then return nil, "failed to create lock:" .. err end
   
+  ok, err = lock:lock(self.TARGET_LIST_LOCK_KEY)
+  if not ok then return nil, "failed to acquire lock: " .. err end
+  
+  target_list, err = self.shm.get(self.TARGET_LIST_KEY)
+  if err then
+    err = "failed to fetch target_list from shm: " .. err
+
+  else
+    if target_list then
+      target_list = deserialize(targetlist)
+    else
+      target_list = {}
+    end
+
+    -- check whether we already have this target
+    for _, target in ipairs(target_list) do
+      if target.ip == ip and target.port == port then
+        err = ("target '%s:%s' already exists"):format(ip, port)
+        break
+      end
+    end
+
+    if not err then
+      -- target does not exist, go add it
+      target_list[#target_list + 1] = {
+        ip = ip,
+        port = port,
+      }
+      target_list = serialize(target_list)
+
+      ok, err = self.shm.set(self.TARGET_LIST_KEY, target_list)
+      if not ok then
+        err = "failed to store target_list in shm: " .. err
+      end
+
+      if not err then
+        -- add health status to shm
+        ok, err2 = self.shm.set(self.TARGET_STATUS:format(ip, port), healthy)
+        if not ok then
+          -- err2: not failing this routine, just log the error and continue
+          self.log(ERR, "failed to set initial health status in shm: ", err2)
+        end
+
+        -- raise event for our newly added target
+        if healthy then
+          self.raise_event(self.events.healthy, ip, port)
+        else
+          self.raise_event(self.events.unhealthy, ip, port)
+        end
+      end
+    end
+  end
+  
+  ok, err2 = lock:unlock()
+  if not ok then 
+    -- err2: not failing this routine, just log the error and continue
+    self.log(ERR, "failed to release lock '", self.TARGET_LIST_LOCK_KEY,
+                  "': ", err2)
+  end
+
+  return err and nil, err
 end
-  
+
+
 --- Remove a target from the healthchecker.
+-- The target not existing is not considered an error.
 -- @param ip ip-address of the target being checked
 -- @param port the port being checked against
 -- @return true on success, nil+err on failure
 function checker:remove_target(ip, port)
+  ip   = tostring(assert(ip, "no ip address provided"))
+  port = tostring(assert(ip, "no port number provided"))
+  healthy = not not healthy   -- force to boolean
+
+  local lock, err, ok, err2, target_list
   
-  -- TODO: implement
+  lock, err = resty_lock.new(self.shm, {
+                exptime = 10,  -- timeout after which lock is released anyway
+                timeout = 5,   -- max wait time to acquire lock
+              })
+  if not lock then return nil, "failed to create lock:" .. err end
   
+  ok, err = lock:lock(self.TARGET_LIST_LOCK_KEY)
+  if not ok then return nil, "failed to acquire lock: " .. err end
+  
+  target_list, err = self.shm.get(self.TARGET_LIST_KEY)
+  if err then
+    err = "failed to fetch target_list from shm: " .. err
+
+  else
+    if target_list then
+      target_list = deserialize(targetlist)
+    else
+      target_list = {}
+    end
+
+    -- find the target
+    local target_found
+    for i, target in ipairs(target_list) do
+      if target.ip == ip and target.port == port then
+        target_found = target
+        table.remove(target_list, i)
+        break
+      end
+    end
+
+    if target_found then
+      -- go update the shm
+      target_list = serialize(target_list)
+
+      ok, err = self.shm.set(self.TARGET_LIST_KEY, target_list)
+      if not ok then
+        err = "failed to store target_list in shm: " .. err
+      end
+
+      if not err then
+        -- remove health status from shm
+        ok, err2 = self.shm.set(self.TARGET_STATUS:format(ip, port), nil)
+        if not ok then
+          -- err2: not failing this routine, just log the error and continue
+          self.log(ERR, "failed to remove health status from shm: ", err2)
+        end
+
+        -- raise event for our removed target
+        self.raise_event(self.events.remove, ip, port)
+      end
+    end
+  end
+  
+  ok, err2 = lock:unlock()
+  if not ok then 
+    -- err2: not failing this routine, just log the error and continue
+    self.log(ERR, "failed to release lock '", self.TARGET_LIST_LOCK_KEY,
+                  "': ", err2)
+  end
+
+  return err and nil, err
 end
+
+
 
 --============================================================================
 -- Status management
@@ -165,6 +330,8 @@ function checker:set_target_status(ip, port, enabled)
 end
 ]]
 
+
+
 --============================================================================
 -- Health management
 --============================================================================
@@ -181,6 +348,7 @@ function checker:report_failure(ip, port)
   
 end
 
+
 --- Report a health success.
 -- Reports a health success which will count against the number of occurences
 -- required to make a target "fall" or "rise".
@@ -192,6 +360,7 @@ function checker:report_success(ip, port)
   -- TODO: implement
   
 end
+
 
 --- Report a http response code.
 -- How the code is interpreted is based on the fall and rise strategies, if it
@@ -205,6 +374,7 @@ function checker:report_http_status(ip, port, req_status)
   
 end
 
+
 -- TODO check what kind of information we get from the OpenResty layer
 -- in order to tell these error conditions apart
 -- https://github.com/openresty/lua-resty-core/blob/master/lib/ngx/balancer.md#get_last_failure
@@ -214,6 +384,7 @@ function checker:report_tcp_failure(ip, port)
   
 end
 
+
 function checker:report_timeout(ip, port)
   
   -- TODO: implement
@@ -221,8 +392,57 @@ function checker:report_timeout(ip, port)
 end
 
 
+
 --============================================================================
--- Timer callbacks
+-- Healthcheck runner
+--============================================================================
+
+-- Runs a single healthcheck probe
+function checker:run_single_check(ip, port)
+  
+  -- TODO: implement
+  
+end
+
+-- executes a work package (a list of checks) sequentially
+function checker:run_work_package(work_package)
+  for _, target in ipairs(work_package) do
+    self:run_single_check(work_package.ip, work_package.port)
+  end
+end
+
+-- runs the active healthchecks concurrently, in multiple work packages.
+-- @param list the list of targets to check
+function checker:active_check_targets(list)
+  local idx = 1
+  local work_packages = {}
+  for _, target in ipairs(list) do
+    local package = work_packages[idx]
+    if not package then
+      package = {}
+      work_packages[idx] = package
+    end
+    package[#package + 1] = target
+    idx = idx + 1
+    if idx > self.concurrency then idx = 1 end
+  end
+  
+  -- hand out work-packages to the threads, note the "-1" because this timer
+  -- thread will handle the last package itself.
+  for i = 1, #work_packages - 1 do
+    work_packages[i].thread = ngx.thread.spawn(self.run_work_package, self, work_packages[i])
+  end
+  -- run last package myself
+  self:run_work_package(work_packages[#work_packages])
+  
+  -- wait for everybody to finish
+  for i = 1, #work_packages - 1 do
+    ngx.thread.wait(work_packages[i])
+  end
+end
+
+--============================================================================
+-- Callbacks, timers and events
 --============================================================================
 -- The timer callbacks are responsible for checking the status, upon success/
 -- failure they will call the health-management functions to deal with the
@@ -232,11 +452,24 @@ end
 function checker.healthy_callback(premature, self)
   if premature or self.stop then
     self.timer_count = self.timer_count - 1
-    return end
+    return
   end
-    
-  -- TODO: implement
+
+  -- create a list of targets to check, here we can still do this atomically
+  local list_to_check = {}
+  for _, target in ipairs(self.targets) do
+    if target.healthy then  -- only healthy ones
+      list_to_check[#list_to_check + 1] = { ip = target.ip, port = target.port }
+    end
+  end
   
+  if not list_to_check[1] then
+    self:log(DEBUG, "checking healthy targets: nothing to do")
+  else
+    self:log(DEBUG, "checking healthy targets: #", #list_to_check)
+    self:active_check_targets(list_to_check)
+  end
+
   -- reschedule timer
   local ok, err = utils.gctimer(self.interval_healthy, self.healthy_callback, self)
   if not ok then
@@ -246,15 +479,29 @@ function checker.healthy_callback(premature, self)
   end
 end
 
+
 -- Timer callback to check the status of currently UNHEALTHY targets
 function checker.unhealthy_callback(premature, self)
   if premature or self.stop then
     self.timer_count = self.timer_count - 1
-    return end
+    return
   end
-    
-  -- TODO: implement
+
+  -- create a list of targets to check, here we can still do this atomically
+  local list_to_check = {}
+  for _, target in ipairs(self.targets) do
+    if not target.healthy then  --- only unhealthy ones
+      list_to_check[#list_to_check + 1] = { ip = target.ip, port = target.port }
+    end
+  end
   
+  if not list_to_check[1] then
+    self:log(DEBUG, "checking unhealthy targets: nothing to do")
+  else
+    self:log(DEBUG, "checking unhealthy targets: #", #list_to_check)
+    self:active_check_targets(list_to_check)
+  end
+
   -- reschedule timer
   local ok, err = utils.gctimer(self.interval_healthy, self.healthy_callback, self)
   if not ok then
@@ -264,6 +511,52 @@ function checker.unhealthy_callback(premature, self)
   end
 end
 
+
+-- Event handler callback
+function checker:event_handler(event_name, ip, port)
+
+  local target_found = (self.targets[ip] or EMPTY)[port]
+  
+  if event_name == self.events.remove then
+    if target_found then
+      -- remove hash part
+      self.targets[target_found.ip][target_found.port] = nil
+      if not next(self.targets[target_found.ip]) then
+        -- no more ports on this ip, so delete it
+        self.targets[target_found.ip] = nil
+      end
+      -- remove from list part
+      for i, target in ipairs(self.targets) do
+        if target.ip == ip and target.port == port then
+          table.remove(self.targets, i)
+          break
+        end
+      end
+      self:log(DEBUG, "event received: target '", ip, ":", port, "' removed")
+
+    else
+      self:log(WARN, "event received to remove an unknown target '", ip, ":", port)
+    end
+
+  elseif event_name == self.events.healthy or 
+         event_name == self.events.unhealthy then
+    if not target_found then
+      -- it is a new target, must add it first
+      self.targets[target_found.ip] = self.targets[target_found.ip] or {}
+      self.targets[target_found.ip][target_found.port] = target
+      self:log(DEBUG, "event received: target added ", ip, ":", port)
+    end
+    local health = (event_name == self.events.healthy)
+    self:log(DEBUG, "event received target status '", ip, ":", port,
+                    "' from '", target.healthy, "' to '", health, "'")
+    target_found.healthy = health
+
+  else
+    self:log(WARN, "unknown event received: ", event_name)
+  end
+end
+
+
 --============================================================================
 -- Miscellaneous
 --============================================================================
@@ -271,20 +564,31 @@ end
 -- Log a message specific to this checker
 -- @param level standard ngx log level constant
 function checker:log(level, ...)
-  ngx_log(level, LOG_PREFIX, "(", self.name, ") " ...)
+  ngx_log(level, LOG_PREFIX, "(", self.name, ") ", ...)
 end
+
+
+-- Raises an event for a target status change.
+function checker:raise_event(event_name, ip, port)
+
+  -- TODO implement, depends on event lib
+
+end
+
 
 --- Stop the background health checks.
 -- @return true
 function checker:stop()
   self.stop = true
+  --TODO: unregister event handler, to be eligible for GC
   return true
 end
+
 
 --- Starts the background health checks.
 -- @return true or nil+error
 function checker:start()
-  if not self.timer_count = 0 then
+  if not self.timer_count == 0 then
     return nil, "cannot start, " .. self.timer_count .. " (of 2) timers are still running"
   end
   
@@ -301,9 +605,13 @@ function checker:start()
   end
   self.timer_count = self.timer_count + 1
 
+  --TODO: unregister and re-register event handler
+
   self.stop = false  -- do this at the end, so if either creation fails, the other stops also
   return true
 end
+
+
 
 --============================================================================
 -- Create health-checkers
@@ -312,7 +620,7 @@ end
 --[[
   {
       name = "some unique name"                                   -- needed as key in shm
-      shm = "healthcheck"   																			-- hidden in worker-events??
+      shm_name = "healthcheck"   																  -- hidden in worker-events??
       type = "http"                                               -- http, https, tcp
       http_req = "GET /status HTTP/1.0\r\nHost: foo.com\r\n\r\n"  -- raw request?
       interval_healthy = 2000,                                    -- run the check cycle every 2 sec on healthy nodes 
@@ -334,23 +642,76 @@ end
 
 --- Creates a new health-checker instance.
 -- @param opts table with checker options
--- @return checker object
+-- @return checker object, or nil + error
 local function new(opts)
-  local obj = {}
+  local self = {
+    -- options defaults
+    concurrency = 10,    -- how many concurrent requests while probing
+    -- other properties
+    targets = nil,   -- list of targets, initially loaded, maintained by events
+    events = nil,    -- hash table with supported events (prevent magic strings)
+    stop = true,     -- flag to idicate to timers to stop checking
+    timer_count = 0, -- number of running timers
+    shm = nil,       -- the shm to use (actual shm, not its name)
+  }
+  
+  assert(opts.shm_name, "required option 'shm_name' is missing")
+  self.shm = ngx.shared[tostring(opts.shm_name)]
+  assert(self.shm, ("no shm found by name '%s'"):format(opts.shm_name))
   
   -- TODO: implement
   
-  -- decorate with methods
+  -- decorate with methods and constants
+  self.events = EVENTS
   for k,v in pairs(checker) do
-    obj[k] = v
+    self[k] = v
   end
   
-  -- start timers
-  obj.stop = true      -- flag to instruct timers to exit
-  obj.timer_count = 0  -- number of timers running
-  obj:start()
+  -- prepare shm keys
+  self.TARGET_STATUS        = SHM_PREFIX .. self.name .. ":%s:%s:status"
+  self.TARGET_LIST_KEY      = SHM_PREFIX .. self.name .. ":target_list"
+  self.TARGET_LIST_LOCK_KEY = SHM_PREFIX .. self.name .. ":target_list_lock"
   
-  return obj
+  -- register for events, and directly after load initial target list
+  -- order is important!
+  -- TODO: implementation depends on the actual event library used
+  event_lib.register(
+    function(...)
+      -- just a wrapper to be able to access `self` as a closure
+      return self:event_handler(event_name, ip, port)
+    end
+  )
+  -- Now load the initial list of targets
+  do
+    -- all atomic accesses, so we do not need a lock
+    self.targets, err = self.shm.get(self.TARGET_LIST_KEY)
+    if err then
+      return nil, "failed to fetch target_list from shm: " .. err
+    end
+    if self.targets then
+      self.targets = deserialize(self.targets)
+    else
+      self.targets = {}
+    end
+    -- load individual statusses
+    for _, target in ipairs(self.targets) do
+      target.healthy = self.shm.get(self.TARGET_STATUS:format(target.ip, target.port))
+      -- fill-in the hash part for easy lookup
+      self.targets[target.ip] = self.targets[target.ip] or {}
+      self.targets[target.ip][target.port] = target
+    end
+    -- TODO: here we need to poll for events, so any changes between fetching the
+    -- list and reading the individual statusses get synced.
+  end
+
+  -- start timers
+  ok, err = self:start()
+  if not ok then
+    --TODO: unregister event handler, to be eligible for GC
+    return nil, err
+  end
+  
+  return self
 end
 
 
